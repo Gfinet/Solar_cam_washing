0f0c5432821c03558cbee16f2cb7b22f9bb07324
gfinet <guylianfinet@hotmail.com>
2025-12-15 15:54:02 +0100

    trans solo start

---
+++ gears/webapp/src/test.js
@@ -0,0 +1,28 @@
+// console.log("Hello World !")
+
+const express = require('express')
+
+const app = express()
+const port = 4243
+
+i = 0
+msg = ""
+while (i < 10)
+{
+    msg += "Hello World !";
+    i++;
+}
+
+app.get('/', (req, res) => res.send(msg))
+
+app.listen(port, () => console.log('Notre app demarre sur http://localhost:',port))
+
+
+
+
+//node -v
+//npm init (creer un package.json)
+//npm install express --save (express sert pour les serveur http)
+//npm install --save-dev nodemon (nodemon permet la relance du programme apres chaque modification du code)
+
+//npm run start
