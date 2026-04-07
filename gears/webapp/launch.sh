c0f4e2bef1ccc6cdf07aaa1bf9b0bc48f1c7525e
Xayling <guylianfinet@ehotmail.com>
2026-04-07 15:12:33 +0200

    little advance

---
+++ gears/webapp/launch.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -f "package-lock.json" ]; then
+	npm ci;
+else
+	npm i;
+fi
+
+echo "URL:" $URL
+
+if [ "$APP_MODE" = "dev" ]; then
+	echo "DEV";
+	exec npm run dev;
+elif [ "$APP_MODE" = "prod" ]; then
+	echo "START";
+	if [ -d "app/frontend/dist" ]; then
+		rm -rf "app/frontend/dist";
+	fi
+	npm run build;
+	exec npm start;
+fi
