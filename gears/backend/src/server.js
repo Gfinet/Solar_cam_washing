f18fc789a23dcf87731a627aac87bbbc3b6b24ec
Xayling <guylianfinet@ehotmail.com>
2026-04-13 22:11:29 +0200

    modbus get solar watt value

--- gears/backend/src/server.js
+++ gears/backend/src/server.js
@@ -9,9 +9,9 @@ import routes from './routes/index.js'
 const serverOn = async () => {
     const server = Fastify({logger: true})
 
+    await server.register(routes, { prefix: '/api' });
     await server.register(prisma);
     await server.register(mb);
-    await server.register(routes, { prefix: '/api' });
 
     server.get('/api', function (request, reply) {
         reply.send({ hello: 'world' })
