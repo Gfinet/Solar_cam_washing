f047ef02760e5a63e79f4280cbc2596c503b56e7
Xayling <guylianfinet@ehotmail.com>
2026-04-18 02:40:59 +0200

    weather save and store into db

--- gears/backend/src/plugins/meteo.js
+++ gears/backend/src/plugins/meteo.js
@@ -1,48 +1,53 @@
 import fp from 'fastify-plugin'
 import { fetchWeatherApi } from "openmeteo";
+import cron from 'node-cron';
 
 
 export default fp(async (server) => {
-    const params = {
-        latitude: 50.89,
-        longitude: 4.37,
-        hourly: "temperature_2m",
-    };
-    const url = "https://api.open-meteo.com/v1/forecast";
-    // const responses = await fetchWeatherApi(url, params);
-    const responses = await fetch("https://api.open-meteo.com/v1/forecast?latitude=50.89&longitude=4.37&hourly=temperature_2m");
+    if (server.prisma.weatherForecast.count === 0)
+        await fetchWeatherData(server);
+    cron.schedule('0 12 * * *', async () => {
+        console.log('Il est midi ! Récupération de la météo...');
+        await fetchWeatherData(server);
+        }, 
+        {
+            timezone: "Europe/Paris" // Très important pour le changement d'heure !
+    });
 
-    // Process first location. Add a for-loop for multiple locations or weather models
-    // const response = responses[0];
-    const data = await responses.json();
-    console.log(data)
-
-    // Attributes for timezone and location
-    // const latitude = response.latitude();
-    // const longitude = response.longitude();
-    // const elevation = response.elevation();
-    // const utcOffsetSeconds = response.utcOffsetSeconds();
-
-    console.log(
-        `\nCoordinates: ${params.latitude}°N ${params.longitude}°E`,
-        // `\nElevation: ${elevation}m asl`,
-        // `\nTimezone difference to GMT+1: ${utcOffsetSeconds}s`,
-    );
-
-    
-    // const hourly = response.hourly() | null;
-
-    // // Note: The order of weather variables in the URL query and the indices below need to match!
-    // const weatherData = {
-    //     hourly: {
-    //     time: Array.from(
-    //         { length: (Number(hourly.timeEnd()) - Number(hourly.time())) / hourly.interval() }, 
-    //         (_ , i) => new Date((Number(hourly.time()) + i * hourly.interval() + utcOffsetSeconds) * 1000)
-    //     ),
-    //     temperature_2m: hourly.variables(0).valuesArray(),
-    //     },
-    // };
-
-    // // The 'weatherData' object now contains a simple structure, with arrays of datetimes and weather information
-    // console.log("\nHourly data:\n", weatherData.hourly)
+    async function fetchWeatherData(server) {
+        const params = {
+            latitude: 50.89,
+            longitude: 4.37,
+            hourly: "temperature_2m",
+        };
+        const url = "https://api.open-meteo.com/v1/forecast";
+        // const responses = await fetchWeatherApi(url, params);
+        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=50.89&longitude=4.37&hourly=temperature_2m&timezone=Europe/Paris");
+        if (!response.ok) throw new Error(data.error || 'Failed to fetch weather data');
+        // Process first location. Add a for-loop for multiple locations or weather models
+        // const response = response[0];
+        const data = await response.json();
+        // setHourlyWeatherResults(data);
+        const hourlyData = data.hourly.time.map((t, index) => {
+            return {
+                time: t,
+                temp: data.hourly.temperature_2m[index]
+            };
+        });
+        console.log(data)
+        for (let i = 0; i < data.hourly.time.length; i++) {
+            await server.prisma.weatherForecast.upsert({
+                where: { 
+                    time: new Date(data.hourly.time[i]) 
+                },
+                update: {
+                    temp: data.hourly.temperature_2m[i]
+                },
+                create: {
+                    time: new Date(data.hourly.time[i]),
+                    temp: data.hourly.temperature_2m[i]
+                }
+            });
+        }
+    }
 })
