c60b5454dc6a172c7c7701ff5272b5d8a17f6df8
Xayling <guylianfinet@ehotmail.com>
2026-06-16 17:24:11 +0200

    factored css style in schedule

--- gears/frontend/src/src/pages/Solar_wash/schedule.jsx
+++ gears/frontend/src/src/pages/Solar_wash/schedule.jsx
@@ -33,91 +33,33 @@ function Schedule() {
   
   return (
     
-    <div style={{ padding: '2rem', textAlign: 'center'}}>
+    <div style={styles.globalDiv}>
       <h1>Bienvenue sur l'espace Parents</h1>
-      <div style={{display:"flex", gap:"1rem", padding: '1rem', justifyContent: 'center', alignItems: 'center'}}>
-      <button style={styles.button} onClick={goToDash}>Revenir à l'acceuil</button>
-      <button style={styles.button} onClick={goToTable}>Voir les tableaux de donnéese</button>
-      <button style={styles.button} onClick={Logout}>Se Déconnecter</button>
+      <div style={styles.buttonDiv}>
+        <button style={styles.button} onClick={goToDash}>Revenir à l'acceuil</button>
+        <button style={styles.button} onClick={goToTable}>Voir les tableaux de donnéese</button>
+        <button style={styles.button} onClick={Logout}>Se Déconnecter</button>
       </div>
-      <div style={{display: 'flex', maxWidth: '1400px', margin: '0 auto', flexDirection: 'row' }}>
-          <MyBarChart
-            data={temp} 
-            valx="time"    
-            valy="sun"
-            unit="w/m2"
-            color="#fbbf24" 
-          />
-          <MyLineChart
-            data={watt} 
-            valx="time"    
-            valy="temperature"
-            unit="w"
-            color="#fbbf24" 
-          />
+      <div style={styles.chartDiv}>
+          <MyBarChart  data={temp} valx="time" valy="sun"         unit="w/m2" color="#fbbf24" />
+          <MyLineChart data={watt} valx="time" valy="temperature" unit="w"    color="#fbbf24" />
       </div>
         
-      <div style={{ 
-        display: 'flex', 
-        justifyContent: 'space-between',
-        textAlign: 'center',
-        alignItems: 'center', 
-        padding: '10px 15px', 
-        borderBottom: '1px solid #eee',
-        backgroundColor: '#fff',
-        borderRadius: '8px',
-        marginBottom: '8px',
-        margin: '0 auto',
-        maxWidth : "50%"
-      }}>
-        
-        <span style={{ fontWeight: 'bold', color: '#000000',  fontSize: '0.9rem' }}>
-            Date
-          </span>
-
-          {/* Type de programme avec une petite icône ou un badge */}
-          <span style={{ fontWeight: 'bold', color: '#000000' }}>
-            Type de programme
-          </span>
-
-          <span style={{ fontWeight: 'bold', color: '#000000' }}>
-            Autheur
-          </span>
-
-          {/* Statut avec couleur dynamique */}
-          <span style={{ 
-            padding: '4px 8px', 
-            borderRadius: '12px', 
-            fontSize: '0.8rem',
-            fontWeight: 'bold',
-            color: '#000000'
-          }}>
-            Terminé?
-          </span>
+      <div style={styles.programTable}>
+        <span style={styles.titleTable}>Date</span>
+        <span style={styles.titleTable}>Type de programme</span>
+        <span style={styles.titleTable}>Auteur</span>
+        <span style={styles.titleTable}>Terminé?</span>
       </div>
 
       {wash.slice(0, 5).map((program) => (
-        <div key={program.id} style={{ 
-          display: 'flex', 
-          justifyContent: 'space-between',
-          textAlign: 'center',
-          alignItems: 'center', 
-          padding: '10px 15px', 
-          borderBottom: '1px solid #eee',
-          backgroundColor: '#fff',
-          borderRadius: '8px',
-          marginBottom: '8px',
-          margin: '0 auto',
-          maxWidth : "50%"
-        }}>
-          {/* Date et Heure formatée */}
+        <div key={program.id} style={styles.programTable}>
           <span style={{ color: '#888', fontSize: '0.9rem' }}>
             {new Date(program.time).toLocaleString('fr-FR', { 
               day: '2-digit', month: '2-digit', hour: '2-digit', minute: '2-digit' 
             })}
           </span>
 
-          {/* Type de programme avec une petite icône ou un badge */}
           <span style={{ fontWeight: 'bold', color: '#555' }}>
             Type {program.type}
           </span>
@@ -126,27 +68,15 @@ function Schedule() {
             par: {program.author.username}
           </span>
 
-          {/* Statut avec couleur dynamique */}
-          <span style={{ 
-            padding: '4px 8px', 
-            borderRadius: '12px', 
-            fontSize: '0.8rem',
+          <span style={{...styles.finishedIndicator, 
             backgroundColor: program.finished ? '#d4edda' : '#fff3cd',
-            color: program.finished ? '#155724' : '#856404'
-          }}>
-            {wash[0].finished ? 'Terminé' : 'En cours'}
+            color: program.finished ? '#155724' : '#856404'}}>
+            {program.finished ? 'Terminé' : 'En cours'}
           </span>
         </div>
       ))}
 
-      <div style={{ 
-        display: 'flex', 
-        flexDirection: 'row', 
-        justifyContent : 'center',
-        maxWidth: '300px', 
-        margin: '0 auto',
-        gap: '1rem'
-        }}>
+      <div style={styles.buttonDiv}>
       <button style={styles.button} onClick={MieleConnect}>Conexion a Miele</button>
       <button style={styles.button} >Programmer une machine</button>
       </div>
@@ -159,13 +89,51 @@ const styles = {
     padding: '10px', 
     background: '#007bff', 
     width: '300px', 
-    color: 'white', 
+    color: 'black', 
     border: 'none', 
     borderRadius: '5px', 
     cursor: 'pointer',
-    height: '50px'
+    height: '50px',
+    fontSize: '100%'
   },
-  
+  globalDiv: { 
+    display: 'flex' , 
+    flexDirection:'column', 
+    padding: '2rem', 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  buttonDiv: {
+    display:"flex", 
+    gap:"1rem", 
+    padding: '1rem', 
+    flexDirection: 'row'
+  },
+  chartDiv: {
+    display: 'flex', 
+    margin: '0 auto', 
+    flexDirection: 'row', 
+    width:"100%"
+  },
+  programTable: { 
+    display: 'flex', 
+    justifyContent: 'space-between',
+    alignItems: 'center', 
+    padding: '10px 15px', 
+    borderBottom: '1px solid #eee',
+    backgroundColor: '#fff',
+    borderRadius: '8px',
+    width : "50%"
+  },
+  titleTable: {
+    fontWeight: 'bold', 
+    color: '#000000'
+  },
+  finishedIndicator: { 
+    padding: '4px 8px', 
+    borderRadius: '12px', 
+    fontSize: '0.8rem',
+  },
 };
 
 export default Schedule
