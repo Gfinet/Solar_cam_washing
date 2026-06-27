cc8e27862a84b8506856722f1546f74b1902abdf
Xayling <guylianfinet@ehotmail.com>
2026-06-27 22:23:50 +0200

    optimise modbus_solar

--- gears/backend/src/plugins/modbus_solar.js
+++ gears/backend/src/plugins/modbus_solar.js
@@ -1,9 +1,6 @@
 import fp from 'fastify-plugin'
-import Modbus from 'jsmodbus'
 import cron from 'node-cron';
 
-// host = ifconfig | grep 192/10 | awk '{print $2}'
-const realTimeRegister = 100;
 
 export default fp (async (server) =>
 {    
@@ -11,7 +8,7 @@ export default fp (async (server) =>
         const response = await fetch('http://192.168.0.194/dyn/login.json', {
             method : 'POST',
             headers : {"Content-Type": "application/json"},
-            body : JSON.stringify({"pass":"6969","right":"usr"})
+            body : JSON.stringify({"pass":Sma.Psw,"right":Sma.Usr})
         })
         const data = await response.json();
         const sid = data?.result?.sid
@@ -24,49 +21,35 @@ export default fp (async (server) =>
             const response = await fetch(`http://192.168.0.194/dyn/getValues.json?sid=${sid}`, {
                 method : 'POST',
                 headers : {"Content-Type": "application/json"},
-                body : JSON.stringify({"destDev":[],"keys":["6100_40263F00", "6400_00262200", "6380_40251E00"]})
+                body : JSON.stringify({"destDev":[],"keys":[Sma.Day, Sma.Instant]})
             })
-            // console.log("RESP ", response)
-            // console.log("---")
+
             const data = await response.json();
             
             if (data.err) return data
-            console.log("DATA ", data.result[process.env.SMA_ID]["6100_40263F00"]['1'])
-            //La puissance instantanée
-            console.log("DATA ", data.result[process.env.SMA_ID]["6400_00262200"]['1'])
-            //Le rendement du jour
-            /*
-            DATA  {
-                '0156-76BCCE5E': {
-                    '6100_40263F00': { '1': [Array] },
-                    '6400_00262200': { '1': [Array] },
-                    '6380_40251E00': { '1': [Array] }
-                    }
-                }
-            */
-        // console.log("volt")
-            const volt = data.result[process.env.SMA_ID]["6100_40263F00"]["1"];
+            // console.log("Instant Sma ", data.result[Sma.Id][Sma.Instant]['1'])
+            // console.log("Day     Sma ", data.result[Sma.Id][Sma.Day]['1'])
+
             const Val = {
-                total : data.result[process.env.SMA_ID]["6400_00262200"]["1"][0].val,
-                instant : data.result[process.env.SMA_ID]["6100_40263F00"]["1"][0].val,
-                // volt : [volt[0].val, volt[1]]
+                total : data.result[Sma.Id][Sma.Day]["1"][0].val,
+                instant : data.result[Sma.Id][Sma.Instant]["1"][0].val,
             }
-            // console.log("Val", Val)
             return Val
         } catch (error) {
-            // console.log("ERROR", error)
             console.error('Erreur lors de la connexion SMA:\n\t', error.cause.message);
             return null
         }
     }
 
+    const Sma = {
+        Id : process.env.SMA_ID,
+        Instant : process.env.SMA_INSTANT,
+        Day : process.env.SMA_DAY,
+        Usr : process.env.SMA_USR,
+        Psw : process.env.SMA_PSW
+    }
+
     let sid = null
-    const todayStart = new Date();
-    todayStart.setHours(0, 0, 0, 0);
-    const count = await server.prisma.solar_Data.findMany({where: {hour: {gte: todayStart}}});
-      
-    // Lance le fetch initial sans bloquer l'initialisation du plugin
-    fetchSolarData(server).catch(err => console.error('Fetch solaire initial échoué:', err.cause.message));
 
     cron.schedule('*/5 * * * *', async () => {
         fetchSolarData(server).catch(err => console.error('Fetch solaire échoué:', err.cause.message));
@@ -75,18 +58,17 @@ export default fp (async (server) =>
     async function fetchSolarData(server)
     {
         try {
-            const lastRecord = await server.prisma.Solar_Data.findFirst({orderBy: { hour: 'desc' } })
-            
-            // console.log("WATT", lastWatt, lastRecord)
 
             let data = await getValuesSMA(sid);
-            console.log("first check", data)
-            // if(!data)   return;
-            if(data.err)
+            let count = 0;
+            while (data.err)
             {
+                //if sid expired or no sid
                 sid = await connectSMA();
-                // console.log("sid", sid)
                 data = await getValuesSMA(sid);
+                count++
+                if (count >5)
+                    throw Error("Cant connect")
             }
 
             await server.prisma.Solar_Data.create({data: {Watts: data.instant || 0 }})
